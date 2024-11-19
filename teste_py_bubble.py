13e36b38f761c90a76a3b6ece2992dec84f1d083
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-11-18 21:15:54 -0300

    license

--- teste_py_bubble.py
+++ teste_py_bubble.py
@@ -1,3 +1,20 @@
+##   Copyright 2024 Antônio Augusto Maguetta Feitosa, Mateus Felipe da Silveira Vieira
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the Affero GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    Affero GNU General Public License for more details.
+
+#    You should have received a copy of the Affero GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>5.
+
+
+
 from bucket_sort import *
 
 
